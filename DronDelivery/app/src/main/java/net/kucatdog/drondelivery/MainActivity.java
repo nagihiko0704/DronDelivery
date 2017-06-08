47504329c65e09b905e5ffb877211b6689106429
Jihye <jihae0584@gmail.com>
2017-06-08 16:23:15 +0900

    Made 'changeBackground' function that changes the background color of the current temperature window and declare a 'delivery' function that changes the location of the drones

--- DronDelivery/app/src/main/java/net/kucatdog/drondelivery/MainActivity.java
+++ DronDelivery/app/src/main/java/net/kucatdog/drondelivery/MainActivity.java
@@ -1,25 +1,55 @@
 package net.kucatdog.drondelivery;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity
 {
+    public int DESIRED_TEMP = 41;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        View state = (View) getLayoutInflater().inflate(R.layout.state, null);
-        View location = (View) getLayoutInflater().inflate(R.layout.location, null);
-        View temperature = (View) getLayoutInflater().inflate(R.layout.temperature, null);
+    //if button clicked, change background based on input text
+    public void changeBackground(View v)
+    {
+        LinearLayout curtempBox = (LinearLayout)findViewById(R.id.current_temp_box);
+        EditText currtemp = (EditText)findViewById(R.id.current_temp);
 
+        //get input value and convert it to int
+        String temp = currtemp.getText().toString();
+        int inttemp = Integer.parseInt(temp);
 
+        //
+        if (inttemp > DESIRED_TEMP)
+        {
+            curtempBox.setBackgroundColor(Color.RED);
+        }
+        else if (inttemp == DESIRED_TEMP)
+        {
+            curtempBox.setBackgroundColor(Color.YELLOW);
+        }
+        else
+        {
+            curtempBox.setBackgroundColor(Color.BLUE);
+        }
 
     }
 
+    //change drone's location if button clicked, and reset when delivery is done
+    public void delivery(View v)
+    {
+
+
+    }
 
 }
